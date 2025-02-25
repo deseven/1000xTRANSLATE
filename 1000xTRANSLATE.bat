4c0669f3bc537641bc219b1be1a7edd5d74b1f35
Ivan <git@d7.wtf>
2025-02-25 01:05:21 +0100

    updated windows batch file for translations

--- 1000xTRANSLATE.bat
+++ 1000xTRANSLATE.bat
@@ -3,20 +3,22 @@
 cls
 echo Please select an option:
 echo 1. Dump game resources
-echo 2. Build translation
-echo 3. Clean up extracted and modified resources
-echo 4. Install internal dependencies
-echo 5. Check config file
-echo 6. Exit
+echo 2. Translate
+echo 3. Build translation
+echo 4. Clean up extracted and modified resources
+echo 5. Install internal dependencies
+echo 6. Check config file
+echo 7. Exit
 echo.
-set /p choice="Enter your choice (1-6): "
+set /p choice="Enter your choice (1-7): "
 
 if "%choice%"=="1" goto dump
-if "%choice%"=="2" goto build
-if "%choice%"=="3" goto clean
-if "%choice%"=="4" goto init
-if "%choice%"=="5" goto check
-if "%choice%"=="6" goto exit
+if "%choice%"=="2" goto translate
+if "%choice%"=="3" goto build
+if "%choice%"=="4" goto clean
+if "%choice%"=="5" goto init
+if "%choice%"=="6" goto check
+if "%choice%"=="7" goto exit
 
 echo Invalid choice. Please try again.
 pause
@@ -25,6 +27,11 @@ goto menu
 :dump
 echo Running 'npm run dump'...
 call npm run dump
+goto completed
+
+:translate
+echo Running 'npm run translate'...
+call npm run translate
 goto completed
 
 :build
